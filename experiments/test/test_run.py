6824e8e1fba19732392664b625914d3cb93bc91c
Haimo Fang <1679845561@qq.com>
2026-03-12 14:52:08 +0800

    deleted fucked-up setup. exploring ideas from scratch

--- experiments/test/test_run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import numpy as np
-
-def test_environment():
-    x = np.linspace(0,1,10)
-    print("Environment working:", x.mean())
-
-if __name__ == "__main__":
-    test_environment()
